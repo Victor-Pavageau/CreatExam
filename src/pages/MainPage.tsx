5c6db641d9e51f4a6b5aab50b6ec3469559f130e
Victor Pavageau <victor.pavageau@viacesi.fr>
2023-03-22 16:55:23 +0100

    add title in the main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,11 @@
 function MainPage() {
   return (
-    <div>TEST</div>
-  )
+    <div className="flex justify-center">
+      <h1 className="max-w-lg font-semibold">
+        Generative Multiple Choice Questionaries has arrived.
+      </h1>
+    </div>
+  );
 }
 
-export default MainPage
+export default MainPage;
