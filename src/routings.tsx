f3748e1592c8a0bd27463d736964bf5b9f40f31f
Victor Pavageau <victor.pavageau@viacesi.fr>
2023-05-15 22:54:21 +0200

    add auth path to routing

--- src/routings.tsx
+++ src/routings.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import CreateExam from "./pages/CreateExam";
+import Auth from "./pages/Auth";
 
 function RouteHandler(): JSX.Element {
   return (
     <Routes>
       <Route path={"/"} element={<MainPage />} />
       <Route path={"/create-exam"} element={<CreateExam />} />
+      <Route path={"/auth"} element={<Auth />} />
     </Routes>
   );
 }
